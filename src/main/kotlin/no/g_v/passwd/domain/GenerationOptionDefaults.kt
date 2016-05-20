71025d72bc76c08cf0e4f2c51ec6c62a182c330e
Andreas Pedro Garcia-Vedal <ap@g-v.no>
2016-05-21 00:56:48 +0200

    Extracted generation option defaults into a singleton class

---
+++ src/main/kotlin/no/g_v/passwd/domain/GenerationOptionDefaults.kt
@@ -0,0 +1,7 @@
+package no.g_v.passwd.domain
+
+object GenerationOptionDefaults {
+    val numberOfPasswords: Int = 1
+    val minPasswordLength: Int = 8
+    val maxPasswordLength: Int = 14
+}
--- src/main/kotlin/no/g_v/passwd/domain/GenerationOptions.kt
+++ src/main/kotlin/no/g_v/passwd/domain/GenerationOptions.kt
@@ -1,8 +1,8 @@
 package no.g_v.passwd.domain
 
 data class GenerationOptions(val algorithm: Algorithm = Algorithm.PRONOUNCEABLE,
-                             val numberOfPasswords: Int = 1,
-                             val minPasswordLength: Int = 8,
-                             val maxPasswordLength: Int = 14,
+                             val numberOfPasswords: Int = GenerationOptionDefaults.numberOfPasswords,
+                             val minPasswordLength: Int = GenerationOptionDefaults.minPasswordLength,
+                             val maxPasswordLength: Int = GenerationOptionDefaults.maxPasswordLength,
                              val symbolsets: Symbolsets = Symbolsets()) {
 }
