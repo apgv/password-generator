0f5f54b0ac25f4c0256dbe50e6d21d4e083b5ccb
Andreas Pedro Garcia-Vedal <ap@g-v.no>
2016-05-21 22:00:31 +0200

    Added constraints on the parameters of ApgOptions

--- src/main/kotlin/no/g_v/passwd/domain/ApgOptions.kt
+++ src/main/kotlin/no/g_v/passwd/domain/ApgOptions.kt
@@ -1,8 +1,11 @@
 package no.g_v.passwd.domain
 
+import javax.validation.constraints.Max
+import javax.validation.constraints.Min
+
 data class ApgOptions(val algorithm: Algorithm = Algorithm.PRONOUNCEABLE,
-                      val numberOfPasswords: Int = ApgOptionDefaults.numberOfPasswords,
-                      val minPasswordLength: Int = ApgOptionDefaults.minPasswordLength,
-                      val maxPasswordLength: Int = ApgOptionDefaults.maxPasswordLength,
+                      @get:[Min(1) Max(10)] val numberOfPasswords: Int = ApgOptionDefaults.numberOfPasswords,
+                      @get:Min(8) val minPasswordLength: Int = ApgOptionDefaults.minPasswordLength,
+                      @get:Max(24) val maxPasswordLength: Int = ApgOptionDefaults.maxPasswordLength,
                       val symbolsets: Symbolsets = Symbolsets()) {
 }
--- src/test/kotlin/no/g_v/passwd/domain/ApgOptionsTest.kt
+++ src/test/kotlin/no/g_v/passwd/domain/ApgOptionsTest.kt
@@ -2,8 +2,11 @@ package no.g_v.passwd.domain
 
 import org.assertj.core.api.Assertions.assertThat
 import org.junit.Test
+import javax.validation.Validation
 
 class ApgOptionsTest {
+
+    private val validator = Validation.buildDefaultValidatorFactory().validator
 
     @Test
     fun default_values() {
@@ -15,4 +18,17 @@ class ApgOptionsTest {
         assertThat(generationOptions.maxPasswordLength).isEqualTo(14)
         assertThat(generationOptions.symbolsets).isEqualTo(Symbolsets())
     }
+
+    @Test
+    fun validate_constraints() {
+        assertThat(hasConstraintViolations(ApgOptions())).isFalse()
+
+        assertThat(hasConstraintViolations(ApgOptions(numberOfPasswords = 0))).isTrue()
+        assertThat(hasConstraintViolations(ApgOptions(numberOfPasswords = 11))).isTrue()
+        assertThat(hasConstraintViolations(ApgOptions(minPasswordLength = 7))).isTrue()
+        assertThat(hasConstraintViolations((ApgOptions(maxPasswordLength = 25)))).isTrue()
+    }
+
+    private fun hasConstraintViolations(apgOptions: ApgOptions) = validator.validate(apgOptions).isNotEmpty()
+
 }
