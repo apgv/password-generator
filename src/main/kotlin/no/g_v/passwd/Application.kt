b9d5f5e28359e471d3e0f7be0fc8a7254d9033e6
Andreas Pedro Garcia-Vedal <ap@g-v.no>
2016-05-22 02:07:54 +0200

    Added validator to application

--- src/main/kotlin/no/g_v/passwd/Application.kt
+++ src/main/kotlin/no/g_v/passwd/Application.kt
@@ -2,10 +2,17 @@ package no.g_v.passwd
 
 import org.springframework.boot.SpringApplication
 import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.context.annotation.Bean
+import org.springframework.validation.beanvalidation.LocalValidatorFactoryBean
+import javax.validation.Validator
 
 @SpringBootApplication
 open class Application {
 
+    @Bean
+    open fun validator(): Validator {
+        return LocalValidatorFactoryBean()
+    }
 }
 
 fun main(args: Array<String>) {
