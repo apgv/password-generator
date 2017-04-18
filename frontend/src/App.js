920b76ad5c28683099018e62909685a1c0e45e85
Andreas Pedro Garcia-Vedal <ap@foobar.codes>
2017-04-18 20:34:40 +0200

    Added UI to application and removed unused CSS

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,21 +1,53 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react';
 import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
-    );
-  }
+    constructor(props) {
+        super(props);
+        this.state = {password: '', pendingRequest: false};
+
+        this.fetchPassword = this.fetchPassword.bind(this);
+    }
+
+    componentDidMount() {
+        this.fetchPassword();
+    }
+
+    fetchPassword() {
+        this.setState({pendingRequest: true});
+
+        fetch('/password')
+            .then(response => response.json())
+            .then(result => {
+                    let s = result[0];
+                    this.setState({password: s.join(''), pendingRequest: false})
+                }
+            );
+    }
+
+    render() {
+        const divStyle = {marginTop: 10};
+        const buttonStyle = {marginLeft: 10};
+
+        return (
+            <div className="App">
+                <div className="App-header">
+                    <img src={logo} className="App-logo" alt="logo"/>
+                    <h2>Generate password</h2>
+                </div>
+                <div style={divStyle}>
+                    Password: {this.state.password}
+                    <button
+                        onClick={this.fetchPassword}
+                        disabled={this.state.pendingRequest}
+                        style={buttonStyle}>
+                        New password
+                    </button>
+                </div>
+            </div>
+        );
+    }
 }
 
 export default App;
