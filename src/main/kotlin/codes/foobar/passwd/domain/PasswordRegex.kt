f602c15f36abbf505f76f34968ebe05d891dbc15
Andreas Pedro Garcia-Vedal <ap@foobar.codes>
2017-05-14 20:57:17 +0200

    Refactoring of class that can generate password regex

--- src/main/kotlin/codes/foobar/passwd/domain/PasswordRegex.kt
+++ src/main/kotlin/codes/foobar/passwd/domain/PasswordRegex.kt
@@ -1,26 +1,22 @@
 package codes.foobar.passwd.domain
 
-class PasswordRegex(val lowerCase: Boolean, val upperCase: Boolean, val numbers: Boolean, val special: Boolean) {
+object PasswordRegex {
 
-    companion object {
-        val A_Z_LOWER_REGEX = "a-z"
-        val A_Z_UPPER_REGEX = "A-Z"
-        val NUMBER_REGEX = "0-9"
-        val SPECIAL_REGEX = "!\"#$%&\\U+0027()*+,-./:;<=>?@[\\]^_`{|}~"
-        val FULL_REGEX = A_Z_LOWER_REGEX + A_Z_UPPER_REGEX + NUMBER_REGEX + SPECIAL_REGEX
-    }
+    val A_Z_LOWER_REGEX = "a-z"
+    val A_Z_UPPER_REGEX = "A-Z"
+    val NUMBER_REGEX = "0-9"
+    val SPECIAL_REGEX = "!\"#$%&\\U+0027()*+,-./:;<=>?@[\\]^_`{|}~"
+    val FULL_REGEX = A_Z_LOWER_REGEX + A_Z_UPPER_REGEX + NUMBER_REGEX + SPECIAL_REGEX
 
-    fun pattern() = if (allFalse()) FULL_REGEX else createPattern()
+    fun pattern(lowerCase: Boolean, upperCase: Boolean, numbers: Boolean, special: Boolean) =
+            if (lowerCase || upperCase || numbers || special) {
+                regexOrEmpty(lowerCase, A_Z_LOWER_REGEX) +
+                        regexOrEmpty(upperCase, A_Z_UPPER_REGEX) +
+                        regexOrEmpty(numbers, NUMBER_REGEX) +
+                        regexOrEmpty(special, SPECIAL_REGEX)
+            } else {
+                FULL_REGEX
+            }
 
-    private fun allFalse() = !lowerCase && !upperCase && !numbers && !special
-
-    private fun createPattern() = lowerCase() + upperCase() + numbers() + special()
-
-    private fun lowerCase() = if (lowerCase) A_Z_LOWER_REGEX else ""
-
-    private fun upperCase() = if (upperCase) A_Z_UPPER_REGEX else ""
-
-    private fun numbers() = if (numbers) NUMBER_REGEX else ""
-
-    private fun special() = if (special) SPECIAL_REGEX else ""
+    private fun regexOrEmpty(b: Boolean, regex: String) = if (b) regex else ""
 }
--- src/test/kotlin/codes/foobar/passwd/domain/PasswordRegexTest.kt
+++ src/test/kotlin/codes/foobar/passwd/domain/PasswordRegexTest.kt
@@ -1,9 +1,9 @@
 package codes.foobar.passwd.domain
 
-import codes.foobar.passwd.domain.PasswordRegex.Companion.A_Z_LOWER_REGEX
-import codes.foobar.passwd.domain.PasswordRegex.Companion.A_Z_UPPER_REGEX
-import codes.foobar.passwd.domain.PasswordRegex.Companion.NUMBER_REGEX
-import codes.foobar.passwd.domain.PasswordRegex.Companion.SPECIAL_REGEX
+import codes.foobar.passwd.domain.PasswordRegex.A_Z_LOWER_REGEX
+import codes.foobar.passwd.domain.PasswordRegex.A_Z_UPPER_REGEX
+import codes.foobar.passwd.domain.PasswordRegex.NUMBER_REGEX
+import codes.foobar.passwd.domain.PasswordRegex.SPECIAL_REGEX
 import org.assertj.core.api.Assertions.assertThat
 import org.junit.Test
 
@@ -11,31 +11,31 @@ class PasswordRegexTest {
 
     @Test
     fun lower_case() {
-        assertThat(PasswordRegex(lowerCase = true, upperCase = false, numbers = false, special = false).pattern())
+        assertThat(PasswordRegex.pattern(lowerCase = true, upperCase = false, numbers = false, special = false))
                 .isEqualTo(A_Z_LOWER_REGEX)
     }
 
     @Test
     fun upper_case() {
-        assertThat(PasswordRegex(lowerCase = false, upperCase = true, numbers = false, special = false).pattern())
+        assertThat(PasswordRegex.pattern(lowerCase = false, upperCase = true, numbers = false, special = false))
                 .isEqualTo(A_Z_UPPER_REGEX)
     }
 
     @Test
     fun number() {
-        assertThat(PasswordRegex(lowerCase = false, upperCase = false, numbers = true, special = false).pattern())
+        assertThat(PasswordRegex.pattern(lowerCase = false, upperCase = false, numbers = true, special = false))
                 .isEqualTo(NUMBER_REGEX)
     }
 
     @Test
     fun special() {
-        assertThat(PasswordRegex(lowerCase = false, upperCase = false, numbers = false, special = true).pattern())
+        assertThat(PasswordRegex.pattern(lowerCase = false, upperCase = false, numbers = false, special = true))
                 .isEqualTo(SPECIAL_REGEX)
     }
 
     @Test
     fun all_false_gives_full_regex() {
-        assertThat(PasswordRegex(lowerCase = false, upperCase = false, numbers = false, special = false).pattern())
+        assertThat(PasswordRegex.pattern(lowerCase = false, upperCase = false, numbers = false, special = false))
                 .isEqualTo(A_Z_LOWER_REGEX + A_Z_UPPER_REGEX + NUMBER_REGEX + SPECIAL_REGEX)
     }
 }
